3034303fdd05840551451344a9a406e531cd7e07
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-26 21:50:15 +0600

    form design for new post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,118 @@
-import React from 'react';
+import { Button, Paper, TextField, Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import useStyles from "./styles";
+import FileBase from "react-file-base64";
 
 const Form = () => {
-    const classes = useStyles();
-    return (
-        <div>
-            form
+  const classes = useStyles();
+  const [postData, setPostData] = useState({
+    writer: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
+  const handleSubmit = () => {};
+  const clear = () => {
+
+  }
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Writing a Blog</Typography>
+        <TextField
+          name="writer"
+          variant="outlined"
+          label="Writer"
+          fullwidth
+          value={postData.writer}
+          onChange={(e) =>
+            setPostData({
+              ...postData,
+              writer: e.target.value,
+            })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullwidth
+          value={postData.title}
+          onChange={(e) =>
+            setPostData({
+              ...postData,
+              title: e.target.value,
+            })
+          }
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullwidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({
+              ...postData,
+              message: e.target.value,
+            })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullwidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({
+              ...postData,
+              tags: e.target.value,
+            })
+          }
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({
+                ...postData,
+                selectedFile: base64,
+              })
+            }
+          />
         </div>
-    );
+        <Button
+          className={classes.buttonSubmit}
+          variant="container"
+          color="primary"
+          size="large"
+          type="submit"
+          fullwidth
+        >
+            Submit
+        </Button>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullwidth
+        >
+            Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
