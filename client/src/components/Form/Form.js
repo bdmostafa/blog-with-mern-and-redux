0282e729d366d2e62087badb6d9b3494ef40e7b2
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-26 19:30:44 +0600

    folder structure

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useStyles from "./styles";
+
+const Form = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            form
+        </div>
+    );
+};
+
+export default Form;
