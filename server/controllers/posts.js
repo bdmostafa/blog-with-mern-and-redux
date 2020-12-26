737491d6144e5fbc378c3608d2d714f6a68f0534
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-26 18:43:01 +0600

    client init and server, route, controller model created

---
+++ server/controllers/posts.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import PostMessage from '../models/postMessage.js';
+
+const router = express.Router();
+
+// Status codes list for rest api
+// https://www.restapitutorial.com/httpstatuscodes.html
+export const getPosts = async (req, res) => { 
+    try {
+        const postMessages = await PostMessage.find();
+                console.log(postMessages)
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (err) {
+        res.status(409).json({message: err.message})
+    }
+}
+
+export default router;
